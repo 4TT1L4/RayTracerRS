c021959e99939ced5c27e33f83e6a9f02c4e4c7d
4TT1L4 <2914096+4TT1L4@users.noreply.github.com>
2022-08-20 17:40:51 +0200

    Minor improvements
     - Re-formatted the whole code.
     - Fixed the warnings.
     - Changed the code to be rendering three bouncing spheres.

--- src/main.rs
+++ src/main.rs
@@ -1,141 +1,205 @@
-use std::{io::{stdout, Write, Stdout}};
+use chrono::Utc;
+use std::io::{stdout, Stdout, Write};
 
 use crossterm::{
-    ExecutableCommand, QueueableCommand,
-    terminal, cursor, style::{self, Color}, Result
+    cursor,
+    style::{self, Color},
+    terminal, ExecutableCommand, QueueableCommand,
 };
 
 fn main() {
-    let mut stdout : Stdout = stdout();
-    stdout.execute(terminal::Clear(terminal::ClearType::All));
-    stdout.execute(crossterm::cursor::Hide);
-  
-    let mut time: u128 = 0;
+    let mut stdout: Stdout = stdout();
+    stdout
+        .execute(terminal::Clear(terminal::ClearType::All))
+        .expect("Error");
+    stdout.execute(crossterm::cursor::Hide).expect("Error");
+
+    let start: i64 = Utc::now().timestamp_millis();
     loop {
-       render_frame(&stdout, time);
-       time = time + 1;
+        let time: i64 = Utc::now().timestamp_millis() - start;
+        render_frame(&stdout, time);
     }
-  }
+}
 
-fn render_frame(mut stdout: &Stdout, time: u128) {
-    let yMax = 40;
-    let xMax = 100;
+fn render_frame(mut stdout: &Stdout, time: i64) {
+    let y_max = 40;
+    let x_max = 100;
 
-    for y in 0..yMax {
-        for x in 0..xMax {
+    for y in 0..y_max {
+        'outer: for x in 0..x_max {
             stdout.queue(cursor::MoveTo(x, y)).expect("Error");
 
-            // Sphere
-            let cx = 1.75;
-            let cy = 30.0;
-            let cz = -1.2 * (time as f64 / 15.0).sin() + 1.2;
-            let r = 1.5;
+            // Spheres
+            let cx_arr = [1.75, -2.0, 6.0];
+            let cy_arr = [30.0, 62.0, 48.0];
+            let cz_arr = [
+                -1.2 * (time as f64 / 300.0).sin() + 1.2,
+                -1.9 * ((150 + time) as f64 / 300.0).sin() + 1.2,
+                -2.4 * ((240 + time) as f64 / 300.0).sin() + 1.2,
+            ];
+            let r_arr = [1.5, 1.5, 1.5];
+            let color_arr = [[0.75, 0.05, 0.05], [0.05, 0.75, 0.05], [0.05, 0.05, 0.75]];
 
             // Point of View: Origin:
             let x0 = 0.0;
             let y0 = -5.0;
             let z0 = -0.0;
-            
+
             // Point of the ray on the projection plane.
-            let x1 = ((x as f64 * 1.0) - 30.0) / xMax as f64;
+            let x1 = ((x as f64 * 1.0) - 30.0) / x_max as f64;
             let y1 = -1.75;
-            let z1 = ((y as f64 * 1.0) - 15.0) / yMax as f64 / 1.5 + 0.01;
+            let z1 = ((y as f64 * 1.0) - 15.0) / y_max as f64 / 1.5 + 0.01;
 
             // Direction
             let dx = x1 - x0;
-            let dy =  y1 - y0;
+            let dy = y1 - y0;
             let dz = z1 - z0;
 
-            let a =  dx * dx + dy * dy + dz * dz;
-            let b = 2.0 * dx * (x0 - cx) + 2.0 * dy * (y0 - cy) + 2.0 * dz * (z0 - cz);
-            let c = cx * cx + cy * cy + cz * cz + x0 * x0 + y0 * y0 + z0 * z0 + (-2.0 * (cx* x0 + cy *y0 +cz *z0)) - r*r;
+            for s in 0..cx_arr.len() {
+                let cx = cx_arr[s];
+                let cy = cy_arr[s];
+                let cz = cz_arr[s];
+                let r = r_arr[s];
 
-            let no_intersection = 0.0 > (b * b - 4.0 * a * c);
-            if (no_intersection) {
-                // Background plane
+                let a = dx * dx + dy * dy + dz * dz;
+                let b = 2.0 * dx * (x0 - cx) + 2.0 * dy * (y0 - cy) + 2.0 * dz * (z0 - cz);
+                let c = cx * cx
+                    + cy * cy
+                    + cz * cz
+                    + x0 * x0
+                    + y0 * y0
+                    + z0 * z0
+                    + (-2.0 * (cx * x0 + cy * y0 + cz * z0))
+                    - r * r;
+
+                let intersection_with_sphere = 0.0 <= (b * b - 4.0 * a * c);
+                if intersection_with_sphere {
+                    // Distance of nearest intersection
+                    let t = (-b - (b * b - 4.0 * a * c).sqrt()) / (2.0 * a);
+
+                    // Intersection point
+                    let x_intersection = x0 + t * dx;
+                    let y_intersection = y0 + t * dy;
+                    let z_intersection = z0 * t * dz;
+
+                    // Normal vector on the sphere
+                    let normal_x = (x_intersection - cx) / r;
+                    let normal_y = (y_intersection - cy) / r;
+                    let normal_z = (z_intersection - cz) / r;
+
+                    // Light Coordinates
+                    let light_x = -15.0;
+                    let light_y = -15.0;
+                    let light_z = -25.0;
+
+                    // Vector from the the intersection point to the light.
+                    let x_light_x = light_x - x_intersection;
+                    let y_light_y = light_y - y_intersection;
+                    let z_light_z = light_z - z_intersection;
+                    let a = ((x_light_x * x_light_x)
+                        + (y_light_y * y_light_y)
+                        + (z_light_z * z_light_z))
+                        .sqrt();
+
+                    // Vector from the the intersection point to the light.
+                    let x_light_x_norm = x_light_x / a;
+                    let y_light_y_norm = y_light_y / a;
+                    let z_light_z_norm = z_light_z / a;
+
+                    // Dot product:
+                    let fctr = normal_x * x_light_x_norm
+                        + normal_y * y_light_y_norm
+                        + normal_z * z_light_z_norm;
+
+                    let color = color_arr[s];
+                    let ka = 0.15;
+                    let kd = 0.75;
+                    let red = color[0];
+                    let green = color[1];
+                    let blue = color[2];
+
+                    let color_red = ka * red + kd * fctr * red;
+                    let color_green = ka * green + kd * fctr * green;
+                    let color_blue = ka * blue + kd * fctr * blue;
+
+                    stdout
+                        .queue(style::SetBackgroundColor(Color::Rgb {
+                            r: ((color_red * 255.0) as u8),
+                            g: (color_green * 255.0) as u8,
+                            b: (color_blue * 255.0) as u8,
+                        }))
+                        .expect("Error");
+                    stdout.queue(style::Print(" ")).expect("Error");
+
+                    continue 'outer;
+                }
+
+                // No intersection with any shere -> Check background plane
                 let a = 0.0;
                 let b = 0.0;
                 let c = 1.0;
                 let d = 1.0;
-                let t = (a * x0 + b* y0 + c* z0 + d) / (a * dx + b* dy + c* dz);
-                if (t > 0.0) {
-                    // Intersection with the plane
-                    let pix = x0 + dx * t;
-                    let piy = y0 + dy * t;
-                    let piz = z0 + dz * t;
+                let t = (a * x0 + b * y0 + c * z0 + d) / (a * dx + b * dy + c * dz);
+                if t > 0.0 {
+                    // Intersection poiubt with the plane
+                    let plane_intersection_x = x0 + dx * t;
+                    let plane_intersection_y = y0 + dy * t;
+                    // We do not need the z coordinate to calculate the pattern.
+                    // let plane_intersection_z = z0 + dz * t;
 
-                    let dark1 = (0 == ((piy  / 1.2) as i64 % 2));
-                    let dark2 = (0 == ((pix / 1.2) as i64  % 2));
-                    if (dark1) {
-                        if (dark2){
-                            stdout.queue(style::SetBackgroundColor(Color::Rgb { r: 10, g: 10, b: 10 })).expect("Error");
+                    let dark1 = 0 == ((plane_intersection_y / 1.2) as i64 % 2);
+                    let dark2 = 0 == ((plane_intersection_x / 1.2) as i64 % 2);
+                    if dark1 {
+                        if dark2 {
+                            stdout
+                                .queue(style::SetBackgroundColor(Color::Rgb {
+                                    r: 10,
+                                    g: 10,
+                                    b: 10,
+                                }))
+                                .expect("Error");
                         } else {
-                            stdout.queue(style::SetBackgroundColor(Color::Rgb { r: 225, g: 225, b: 225 })).expect("Error");
+                            stdout
+                                .queue(style::SetBackgroundColor(Color::Rgb {
+                                    r: 225,
+                                    g: 225,
+                                    b: 225,
+                                }))
+                                .expect("Error");
+                        }
+                    } else {
+                        if !dark2 {
+                            stdout
+                                .queue(style::SetBackgroundColor(Color::Rgb {
+                                    r: 10,
+                                    g: 10,
+                                    b: 10,
+                                }))
+                                .expect("Error");
+                        } else {
+                            stdout
+                                .queue(style::SetBackgroundColor(Color::Rgb {
+                                    r: 225,
+                                    g: 225,
+                                    b: 225,
+                                }))
+                                .expect("Error");
                         }
                     }
-                    else {
-                        if (!dark2){
-                            stdout.queue(style::SetBackgroundColor(Color::Rgb { r: 10, g: 10, b: 10 })).expect("Error");
-                        } else {
-                            stdout.queue(style::SetBackgroundColor(Color::Rgb { r: 225, g: 225, b: 225 })).expect("Error");
-                        }
-                    }
-                }
-                else {
+                } else {
                     // No intersecrtion with the plane -> draw sky.
-                    stdout.queue(style::SetBackgroundColor(Color::Rgb { r: (y*15 % 255) as u8, g: (y*15 % 255) as u8, b: 255 })).expect("Error");
+                    stdout
+                        .queue(style::SetBackgroundColor(Color::Rgb {
+                            r: (y * 15 % 255) as u8,
+                            g: (y * 15 % 255) as u8,
+                            b: 255,
+                        }))
+                        .expect("Error");
                 }
-
-                stdout.queue(style::Print(" ")).expect("Error");
-                continue;
             }
-
-            // Distance of nearest intersection
-            let t = (-b - (b * b - 4.0 * a * c).sqrt()) / (2.0 * a);
-
-            // Intersection point
-            let xI = x0 + t * dx;
-            let yI = y0 + t * dy;
-            let zI = z0 * t * dz;
-
-            // Normal vector on the sphere
-            let Nx = (xI - cx) / r;
-            let Ny= (yI - cy) / r;
-            let Nz= (zI - cz) / r;
-
-           // Light Coordinates
-           let Lx = -15.0;
-           let Ly = -15.0;
-           let Lz = -25.0;
-
-            // Vector from the the intersection point to the light.
-            let x_Lx = Lx - xI;
-            let y_Ly = Ly - yI;
-            let z_Lz = Lz - zI;
-            let a = ((x_Lx * x_Lx) + (y_Ly * y_Ly) + (z_Lz * z_Lz)).sqrt();
-            
-            // Vector from the the intersection point to the light.
-            let x_Lx_norm = x_Lx / a;
-            let y_Ly_norm = y_Ly / a;
-            let z_Lz_norm = z_Lz / a;
-
-            // Dot product:
-            let fctr = Nx * x_Lx_norm + Ny * y_Ly_norm + Nz * z_Lz_norm;
-
-            let ka = 0.15;
-            let kd = 0.75;
-            let SR = 0.75;
-            let SG = 0.05;
-            let SB = 0.05;
-
-            let colorR = ka * SR + kd * fctr * SR;
-            let colorG = ka * SG + kd * fctr * SG;
-            let colorB = ka * SB + kd * fctr * SB;
-
-            stdout.queue(style::SetBackgroundColor(Color::Rgb { r: ((colorR * 255.0) as u8), g: (colorG * 255.0) as u8, b: (colorB * 255.0) as u8 })).expect("Error");
             stdout.queue(style::Print(" ")).expect("Error");
+            continue;
         }
-      }
-
-      stdout.flush().expect("Error");
-  }
+        stdout.flush().expect("Error");
+    }
+}
